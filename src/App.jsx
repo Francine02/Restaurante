b32cd9caa14bcf57e9a4be5c87385a25697ded95
Francine19 <francine.c.oliveiracruz@gmail.com>
2024-04-28 14:05:00 -0300

    Adicionando novos recursos e alterando os antigos

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import logo from "./img/icon.ico"
 import cart from "./img/cart.png"
-import sushi from "./img/—Pngtree—ssushi.png"
+import sushi from "./img/combo.png"
 import bg from "./img/background.jpg"
 import {Nav} from "./Nav"
 import "./index.css"
@@ -10,7 +10,7 @@ function App() {
     <>
     <div style={{backgroundImage: `url(${bg})`}} className="bg-cover w-full">
 
-      <header className="w-full h-15 md:px-20 px-3 py-2 flex justify-between fixed bg-stone-900 bg-opacity-65 top-0 start-0 border-b border-stone-700">
+      <header className="w-full h-15 md:px-20 px-3 py-2 flex justify-between fixed bg-black bg-opacity-30">
         <img src={logo} alt="logo" className="w-10"/>
 
         <div className="flex py-1">
@@ -19,8 +19,16 @@ function App() {
         </div>
       </header>
 
-      <div className="pt-16">
-        <img src={sushi}  />
+      <div className="grid grid-cols-2 md:px-20 px-3 md:py-16 py-40 gap-1">
+        <div className="md:py-40">
+          <p className="font-semibold md:text-5xl text-md text-white" id="title">Bem-vindo ao Japa!</p>
+          <p className="text-white md:text-md text-sm pt-5">Descubra no Japa uma fusão de tradição e inovação em cada pedaço de sushi.</p>
+
+          <button type="button" class="mt-8 focus:outline-none text-white font-bold focus:ring-4 focus:ring-[#f25c05b0] rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-[#F25C05]" disabled >Veja o menu</button>
+        </div>
+        <div>
+          <img src={sushi} className="md:pl-12" />
+        </div>
       </div>
     </div>
 
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,7 +1,26 @@
 export function Nav() {
     return (
-      <>
-      </>
-    )
-  }
+    <div className="bg-black text-center text-white pt-20">
+      <p className="font-semibold md:text-4xl text-md text-white" id="title">MENU:</p>
+ 
+      <div className="flex justify-center mt-5">
+        <div class="ml-5">
+          <input id="op-combos" type="radio" name="op-menu" class="w-4 h-4"/>
+          <label htmlFor="op-combos" class="ms-2 text-lg">Combos</label>
+        </div>
+
+        <div class="ml-5">
+          <input id="op-hots" type="radio" name="op-menu" class="w-4 h-4"/>
+          <label htmlFor="op-hots" class="ms-2 text-lg">Quentes</label>
+        </div>
+      </div>
+
+        <div>
+          <input id="op-drinks" type="radio" name="op-menu" class="w-4 h-4"/>
+          <label htmlFor="op-drinks" class="ms-2 text-lg">Bebidas</label>
+        </div>
+
+    </div>
+  )
+}
   
